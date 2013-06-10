d3287b36ba7d3483d3194a4079ffcbcfbcac7a37
Wojtek Oledzki <wojciech.oledzki@bskyb.com>
2013-06-10 07:05:33 +0100

    Adding example JS widget class.
    AMD JS modules works :)

--- scripts/app.js
+++ scripts/app.js
@@ -7,10 +7,28 @@
 define([
 	'lib/ready',
 	'hoborglabs/widget',
+	'examples/testWidget',
 	'hoborglabs/widgetManager'
-], function(ready, Widget, WidgetManager) {
+], function(ready, Widget, TestWidget, WidgetManager) {
 
-	var log = function (level, message) {};
+	// inject log aspect
+	var logWrapper = function(namespace) {
+		return function(level, message) {
+			message = namespace + ': ' + message;
+			if ('debug' == level) {
+				console.debug(message);
+			} else if ('info' == level) {
+				console.info(message);
+			} else {
+				console.log(level + ': ' + message); 
+			}
+		}
+	}
+	var log = logWrapper('app');
+	
+	WidgetManager.prototype.log = logWrapper('Hoborg.WidgetManager');
+	Widget.prototype.log = logWrapper('Hoborg.Widget');
+	TestWidget.prototype.log = logWrapper('Examples.TestWidget');
 
 	// on dom ready...
 	ready(function() {
@@ -25,7 +43,4 @@ define([
 		widgetManager.init();
 		widgetManager.start();
 	});
-
-	// inject log aspect
-	log = function (level, message) { console.log(message); };
 });
