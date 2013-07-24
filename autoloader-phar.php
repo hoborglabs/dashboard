399b2f5888d90a30e02f6588494a85429b88eeed
Wojtek Oledzki <wojciech.oledzki@bskyb.com>
2013-07-24 21:53:39 +0100

    small clean-up and adding `Kernel::addExtensionPath` from develop branch

---
+++ autoloader-phar.php
@@ -0,0 +1,11 @@
+<?php
+
+include_once 'src/Hoborg/Dashboard/Kernel.php';
+include_once 'src/Hoborg/Dashboard/Dashboard.php';
+include_once 'src/Hoborg/Dashboard/Exception.php';
+include_once 'src/Hoborg/Dashboard/Cli.php';
+include_once 'src/Hoborg/Dashboard/IWidgetProvider.php';
+include_once 'src/Hoborg/Dashboard/Proxy.php';
+include_once 'src/Hoborg/Dashboard/StaticAssetsProxy.php';
+include_once 'src/Hoborg/Dashboard/Widget.php';
+include_once 'src/Hoborg/Dashboard/WidgetProvider.php';
