f687d50feb151f4246b8fcb1009f5fd74dce6501
Wojtek Oledzki <wojciech.oledzki@bskyb.com>
2013-07-26 07:19:20 +0100

    updating index-phar

--- htdocs/index-phar.php
+++ htdocs/index-phar.php
@@ -1,21 +1,7 @@
 <?php
-include 'phar://dashboard.phar/autoload.php';
-$pharRoot = 'phar://dashboard.phar';
+$rootPath = __DIR__ . '/..';
+require_once $rootPath . '/vendors/dashboard.phar';
 
-$conf = array();
-if (is_readable(realpath(PHAR_ROOT . '/init.php'))) {
-	$conf = require PHAR_ROOT . '/init.php';
-}
-
-// set-up dashboard and run
-$kernel = new \Hoborg\Dashboard\Kernel($pharRoot);
-
+$kernel = new \Hoborg\Dashboard\Kernel('phar://dashboard.phar');
 $kernel->setDefaultParam('conf', 'demo');
-$kernel->setDefaultParam('public', '/dashboard.phar');
-$kernel->addDefaultParams($conf);
-
-$kernel->addPath('config', array($pharRoot . '/conf', PHAR_ROOT . '/conf'));
-$kernel->addPath('templates', array($pharRoot . '/templates', PHAR_ROOT . '/templates'));
-$kernel->addPath('widgets', array($pharRoot . '/widgets', PHAR_ROOT . '/widgets'));
-
 $kernel->handle(array_merge($_GET, $_POST));
