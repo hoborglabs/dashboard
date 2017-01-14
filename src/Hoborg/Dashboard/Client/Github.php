99dfbdcf5036f25869826a7a87dc82732d33b3ac
David Reed <contact@dave-o-reed.co.uk>
2017-01-14 13:42:54 +0000

    [GITHUB-22] Adds etags to Github requests
    Slightly hacky implementation using apc to store etags with bodies.

--- src/Hoborg/Dashboard/Client/Github.php
+++ src/Hoborg/Dashboard/Client/Github.php
@@ -15,8 +15,46 @@ class Github {
 		$this->baseUrl = $baseUrl;
 	}
 
+	public function checkEtag($url, $etagKey, $etagBodyKey) {
+		if (!extension_loaded('apc')) {
+			return false;
+		}
+
+		$etag = apc_fetch($etagKey);
+		if (false === $etag) {
+			return false;
+		}
+
+		$this->caller->get($url, array("If-None-Match: {$etag}"));
+		if (304 === $this->caller->getResponseCode()) {
+			return apc_fetch($etagBodyKey);
+		}
+
+		return false;
+	}
+
 	public function get($url) {
-		$body = $this->caller->get("{$this->baseUrl}{$url}{$this->getGetParams()}");
+		$headers = array();
+
+		$url = "{$this->baseUrl}{$url}{$this->getGetParams()}";
+	
+		$etagKey = md5('github-etag' . $url);
+		$etagBodyKey = md5('github-etag-body' . $url);
+
+		$body = $this->checkEtag($url, $etagKey, $etagBodyKey);
+
+		if ($body !== false) {
+			return json_decode($body, true);
+		}
+
+		$body = $this->caller->get($url);
+		$etag = $this->caller->getResponseHeader('etag');
+		
+		if (false !== $etag && extension_loaded('apc')) {
+			apc_store($etagKey, $etag);
+			apc_store($etagBodyKey, $body);
+		}
+
 		return json_decode($body, true);
 	}
 
--- src/Hoborg/Dashboard/Client/Http.php
+++ src/Hoborg/Dashboard/Client/Http.php
@@ -3,15 +3,27 @@ namespace Hoborg\Dashboard\Client;
 
 class Http {
 
+	protected $responseCode;
+	protected $headers = array();
+	protected $body;
+
 	public function get($url, array $headers = array()) {
+		$this->responseCode = false;
+		$this->headers = array();
+		$this->body = '';
+
 		$curl = curl_init($url);
 
-		$headers = array("User-Agent: hoborglabs/dashboard");
+		$headers[] = "User-Agent: hoborglabs/dashboard";
 
 		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_HEADERFUNCTION, array($this, 'processHeader'));
+		$this->body = curl_exec($curl);
+		$this->responseCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+		curl_close($curl);
 
-		return curl_exec($curl);
+		return $this->body;
 	}
 
 	public function getQueryString(array $params) {
@@ -29,4 +41,28 @@ class Http {
 		return implode('&', $getQuery);
 	}
 
+	public function getResponseCode() {
+		return $this->responseCode;
+	}
+
+	public function getResponseHeaders() {
+		return $this->headers;
+	}
+
+	public function getResponseHeader($header) {
+		if (array_key_exists($header, $this->headers)) {
+			return $this->headers[$header];
+		}
+		return false;
+	}
+
+	private function processHeader($curl, $headerLine) {
+		if (preg_match('#^HTTP#', $headerLine) === 1) {
+			$this->headers['http_code'] = $headerLine;	
+		} else {
+			list ($key, $value) = explode(': ', $headerLine);
+			$this->headers[strtolower(trim($key))] = trim($value);
+		}
+		return strlen($headerLine);
+	}
 }
